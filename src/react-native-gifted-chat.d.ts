5873e67c14352df203b0f6cdcf0a85f1f7ec57af
Enzo Bomark <71819938+EnzoBomark@users.noreply.github.com>
2023-06-28 10:53:01 +0200

    feat: system messages

---
+++ src/react-native-gifted-chat.d.ts
@@ -0,0 +1,18 @@
+import 'react-native-gifted-chat';
+
+declare module 'react-native-gifted-chat' {
+  interface IMessage {
+    _id: string | number;
+    text: string;
+    createdAt: Date | number;
+    user?: User;
+    image?: string;
+    video?: string;
+    audio?: string;
+    system?: boolean;
+    sent?: boolean;
+    received?: boolean;
+    pending?: boolean;
+    quickReplies?: QuickReplies;
+  }
+}
