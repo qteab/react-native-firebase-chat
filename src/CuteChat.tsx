5bcd03ab9e4ac4f06d5ff95457241d056dea5e13
Melvin <melvin@qte.se>
2025-04-30 10:55:03 +0200

    refactor: extract appendSnapshot to standalone function

--- src/CuteChat.tsx
+++ src/CuteChat.tsx
@@ -55,50 +55,6 @@ export function CuteChat(props: CuteChatProps) {
     [setIsLoading]
   );
 
-  const appendSnapshot = (
-    currentMessages: IMessage[],
-    snapshotChanges: SnapshotChange[]
-  ): IMessage[] => {
-    console.log('Appending snapshot...');
-    const newMessages = [...currentMessages];
-
-    for (const change of snapshotChanges) {
-      switch (change.type) {
-        case 'removed':
-          console.log('Message remove id:', change.message._id);
-          const deleteIndex = newMessages.findIndex(
-            (m) => change.message._id === m._id
-          );
-          if (deleteIndex === -1) {
-            console.log('Message does not exist in currentMessage');
-            break;
-          }
-          newMessages.splice(deleteIndex, 1);
-          console.log('Message removed');
-          break;
-        case 'added':
-        case 'modified':
-          console.log('Message modified or added id:', change.message._id);
-          const modifiedIndex = newMessages.findIndex(
-            (m) => change.message._id === m._id
-          );
-          if (modifiedIndex === -1) {
-            console.log('Message added');
-            newMessages.push(change.message);
-          } else {
-            console.log('Message updated');
-            newMessages[modifiedIndex] = change.message;
-          }
-          break;
-      }
-    }
-
-    return newMessages.sort(
-      (a, b) =>
-        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-    );
-  };
-
   const markMessagesAsRead = useCallback(
     async (newMessages: IMessage[]) => {
       const unreadMessages = newMessages.filter(
@@ -433,5 +389,48 @@ export const prepareSnapshot = async (
       type: change.type,
       message: await docToMessage(change.doc, chatId),
     }))
+  );
+};
+
+export const appendSnapshot = (
+  currentMessages: IMessage[],
+  snapshotChanges: SnapshotChange[]
+): IMessage[] => {
+  console.log('Appending snapshot...');
+  const newMessages = [...currentMessages];
+
+  for (const change of snapshotChanges) {
+    switch (change.type) {
+      case 'removed':
+        console.log('Message remove id:', change.message._id);
+        const deleteIndex = newMessages.findIndex(
+          (m) => change.message._id === m._id
+        );
+        if (deleteIndex === -1) {
+          console.log('Message does not exist in currentMessage');
+          break;
+        }
+        newMessages.splice(deleteIndex, 1);
+        console.log('Message removed');
+        break;
+      case 'added':
+      case 'modified':
+        console.log('Message modified or added id:', change.message._id);
+        const modifiedIndex = newMessages.findIndex(
+          (m) => change.message._id === m._id
+        );
+        if (modifiedIndex === -1) {
+          console.log('Message added');
+          newMessages.push(change.message);
+        } else {
+          console.log('Message updated');
+          newMessages[modifiedIndex] = change.message;
+        }
+        break;
+    }
+  }
+
+  return newMessages.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
   );
 };
